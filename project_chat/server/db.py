4178c37d2e6a476b96d57d1d391182ee1435c3d1
Igor Kuzmin <igorkuz2018@yandex.ru>
2021-04-04 21:31:31 +0300

    db sql

---
+++ project_chat/server/db.py
@@ -0,0 +1,229 @@
+import sqlite3
+import re
+
+
+class UserModel:
+    def __init__(self, url_base):
+        self.url_base = url_base
+        self.namemodel = self.__class__.__name__
+        self.id = 'id  INTEGER primary key'
+        self.username = 'username varchar(30) not NULL UNIQUE'
+        self.password = '`password` varchar(40) not null'
+        self.is_active = 'is_active TINYINT default 0'
+
+        attr = []
+        for name, value in vars(self).items():
+            if not value == self.url_base:
+                if not value == self.namemodel:
+                    attr.append(value)
+        self.str_atr = ', '.join(attr)
+
+    def migrate(self):
+        with sqlite3.connect(self.url_base) as conn:
+            print(f"create table if not exists {self.namemodel} ({self.str_atr});")
+            cursor = conn.cursor()
+            cursor.execute(
+                f"create table if not exists {self.namemodel} ({self.str_atr});")
+
+
+class HistoryClient:
+    def __init__(self, url_base):
+        self.url_base = url_base
+        self.namemodel = self.__class__.__name__
+        self.id_history = 'id INTEGER primary key'
+        self.username_id = 'username_id INT'
+        self.time_at = 'time_at DATETIME'
+        self.ip_addr = 'ip_addr varchar(40) not null'
+        self.foreign_key = 'FOREIGN KEY (username_id) REFERENCES UserModel(id) ON UPDATE CASCADE ON DELETE CASCADE'
+
+        attr = []
+        for name, value in vars(self).items():
+            if not value == self.url_base:
+                if not value == self.namemodel:
+                    attr.append(value)
+        self.str_atr = ', '.join(attr)
+
+    def migrate(self):
+        with sqlite3.connect(self.url_base) as conn:
+            cursor = conn.cursor()
+            print(f"create table if not exists {self.namemodel} ({self.str_atr});")
+            cursor.execute(
+                f"create table if not exists {self.namemodel} ({self.str_atr});")
+
+
+class ClientStorage:
+    def __init__(self, url_base):
+        self.url_base = url_base
+        self.conn = None
+        self.cursor = None
+
+    def initialization(self):
+        conn = sqlite3.connect(self.url_base)
+        cursor = conn.cursor()
+        self.conn = conn
+        self.cursor = cursor
+
+    def add(self, model, parameter):
+        self.initialization()
+        keys = []
+        values = []
+        str_k = '('
+        for key, value in parameter.items():
+            str_k += '?,'
+            keys.append(key)
+            values.append(value)
+        str_k = str_k[:-1]
+        str_k += ')'
+        keys = tuple(keys)
+        values = tuple(values)
+        try:
+            self.cursor.execute(f"INSERT INTO {model}{keys} values {str_k}", values)
+        except BaseException as e:
+            print('Error: add', e)
+        self.conn.commit()
+        self.conn.close()
+
+    def select(self, model, parameters):
+        self.initialization()
+        str_join = ', '.join(parameters)
+        try:
+            self.cursor.execute(f"SELECT {str_join} FROM {model} ")
+            results = self.cursor.fetchall()
+            return results
+        except BaseException as e:
+            print('Error: select', e)
+        self.conn.close()
+
+    def get(self, model, parameters, field):
+        self.initialization()
+        # print(f"SELECT {parameters} FROM {model} ")
+        str_join = ', '.join(parameters)
+        keys = []
+        values = []
+
+        list_filter = []
+
+        for key, value in parameters.items():
+            str_filter = key + '=:' + key
+            list_filter.append(str_filter)
+            keys.append(key)
+            values.append(value)
+
+        filter_join = ' and '.join(list_filter)
+
+        key_join = ', '.join(field)
+
+        try:
+
+            self.cursor.execute(f"SELECT time_at, ip_addr FROM HistoryClient WHERE UserModel.id=1")
+            results = self.cursor.fetchall()
+            print(results)
+            self.cursor.execute(f"SELECT {key_join} FROM {model} WHERE {filter_join}", parameters)
+            results = self.cursor.fetchall()
+
+            # print(results)
+            return results
+        except BaseException as e:
+            print('Error: get', e)
+        self.conn.close()
+
+    def update(self, model, where, parameters):
+        self.initialization()
+
+        filter_update = []
+        filter_where = []
+        for key, value in where.items():
+            str_filter = key + '=:' + key
+            filter_where.append(str_filter)
+
+        for key, value in parameters.items():
+            str_filter = key + '=:' + key
+            filter_update.append(str_filter)
+            where[key] = value
+
+        filter_where = ' and '.join(filter_where)
+        filter_update = ' and '.join(filter_update)
+
+        try:
+            self.cursor.execute(f"UPDATE {model} SET {filter_update} WHERE {filter_where}", where)
+            self.conn.commit()
+
+        except BaseException as e:
+            print('Error: update', e)
+        self.conn.close()
+
+    def join(self, models, field=None, where=None):
+        self.initialization()
+
+        for obj in models:
+            string = getattr(obj, 'foreign_key', False)
+            print(string)
+            if string:
+
+                pattern = r'\((.+?)\)'
+                pattern1 = r'\(([a-z]+)\)'
+                key_id = re.search(pattern, string)
+                key_ref = re.search(pattern1, string)
+
+                key_id = key_id.group(1)
+                key_ref = key_ref.group(1)
+                print(key_id)
+                print(key_ref)
+                model_key = obj.namemodel
+            else:
+                model_ref = obj.namemodel
+                print(model_ref)
+
+        if field != None:
+            key_join = ', '.join(field)
+        else:
+            key_join = '*'
+
+        if where != None:
+            where = f'WHERE {where}'
+        else:
+            where = ''
+
+        try:
+
+            self.cursor.execute(
+                f"SELECT {key_join} FROM {model_ref} INNER JOIN {model_key} ON {model_ref}.{key_ref}={model_key}.{key_id} {where}")
+            results = self.cursor.fetchall()
+            print(results)
+
+            results = self.cursor.fetchall()
+
+            return results
+        except BaseException as e:
+            print('Error: join', e)
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    user = UserModel('company.db3')
+    user.migrate()
+    history = HistoryClient('company.db3')
+    history.migrate()
+
+    name = 'igor'
+    password = '123'
+    list_data = {'username': 'igor1', 'password': '45321'}
+
+    cliendb = ClientStorage('company.db3')
+    cliendb.add('UserModel', list_data)
+
+    cliendb.select('UserModel', ['id', 'username', 'password'])
+
+    dict_get = {'id': 1, 'username': 'igor', 'password': '45321'}
+
+    field = ['username', 'password']
+    update_set = {'is_active': 1}
+    cliendb.get('UserModel', dict_get, field)
+
+    cliendb.update('UserModel', dict_get, update_set)
+
+    history_add = {'username_id': 1, 'time_at': '2015-02-25 13:21:49', 'ip_addr': '127.0.0.1'}
+
+    f = cliendb.select('UserModel', ['id', 'username', 'password'])
+
+    cliendb.join([user, history], field=['UserModel.username'], where='UserModel.id=2')
